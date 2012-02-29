10c9428f9b654d06a07a9c97800c5e01c8ff4a38
Dan Bode <dan@puppetlabs.com>
2012-02-29 14:21:07 -0800

    Add example of testing how facts effect catalogs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,6 @@
 class rspec_test($message = 'default') {
   notify { $message: }
+  if $::some_fact == 'value' {
+    file { '/tmp/some_file': content => 'some contents' }
+  }
 }
--- spec/classes/rspec_test_spec.rb
+++ spec/classes/rspec_test_spec.rb
@@ -7,4 +7,12 @@ describe 'rspec_test' do
     let :params do {:message => 'override'} end
     it { should contain_notify('override') }
   end
+  describe 'when supplying some_fact has value' do
+    let :facts do { :some_fact => 'value' } end
+    it { should contain_file('/tmp/some_file') }
+  end
+  describe 'when some_fact does not have value' do
+    let :facts do { :some_fact => 'not_the_value' } end
+    it { should_not contain_file('/tmp/some_file') }
+  end
 end
